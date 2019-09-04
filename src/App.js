e40574c842add852df9180a03c3519a78a30c252
rajasekhar <rajasekhar.gavidi@gmail.com>
2019-09-04 11:21:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="mainbar">
+        <div className="header">
+          SIM
+          <img
+            href=""
+            alt="sim theme"
+            style={{ width: "500px", height: "300px" }}
+          />
+        </div>
+        <div className="ui frame">
+          <span>Students logins</span>
+          <br />
+          <span>Teachers logins</span> <br />
+          <span>Principal logins</span> <br />
+          <span>Fee logins</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
